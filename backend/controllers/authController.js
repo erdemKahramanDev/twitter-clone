4a7f7aed4b5c06c346e4f4e7b70218b25c397697
Ahmed Erdem Kahraman <ahmederdemkahraman@gmail.com>
2025-10-17 17:33:50 +0300

    Twitter Clone Projesi

---
+++ backend/controllers/authController.js
@@ -0,0 +1,11 @@
+export const signup = async(req, res) => {
+    res.json({ message: "User signed up successfully" });
+};
+
+export const logout = async (req, res) => {
+  res.json({ message: "User signed up successfully" });
+};
+
+export const login = async (req, res) => {
+  res.json({ message: "User signed up successfully" });
+};
